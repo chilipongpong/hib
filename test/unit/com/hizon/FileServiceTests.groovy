4e6d7199891c30ce97c1e2ccffe8a5c67e4f405c
Alec <alecgonzales@gmail.com>
2014-02-12 08:28:01 +0800

    Fixed Tests

--- test/unit/com/hizon/FileServiceTests.groovy
+++ test/unit/com/hizon/FileServiceTests.groovy
@@ -8,7 +8,7 @@ import org.junit.*
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
-@TestFor(ThemeService)
+@TestFor(FileService)
 class FileServiceTests {
 
     void testSomething() {
