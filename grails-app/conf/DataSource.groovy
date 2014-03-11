67195e879d1ee32e7690cb8d2511cd41409059ac
Lorence del Rosario <lorence.delrosario@orangeandbronze.com>
2014-03-11 21:10:24 +0800

    fix for grails war error encountered in deployment + datasource setup in deployment

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -13,7 +13,7 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:mysql://localhost/hib"
             driverClassName = "com.mysql.jdbc.Driver"
             username = "root"
@@ -28,19 +28,11 @@ environments {
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost/hib"
+            driverClassName = "com.mysql.jdbc.Driver"
+            username = "root"
+            password = "root"
         }
     }
 }
