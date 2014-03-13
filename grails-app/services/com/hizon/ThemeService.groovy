07ddf0589954f97b554492bb4a4710518d1e58ed
Alec <alecgonzales@gmail.com>
2014-03-13 19:09:04 +0800

    Added theme service

---
+++ grails-app/services/com/hizon/ThemeService.groovy
@@ -0,0 +1,18 @@
+package com.hizon
+
+class ThemeService {
+
+    def getThemesBasedOnColor(Set<Color> colors) {
+		def result = new HashSet<Theme>()
+		def themes = Theme.findAll()
+		for (Theme theme: themes) {
+			for (Color color: colors) {
+				if (theme.colors.contains(color)) {
+					result.add(theme)
+					break
+				}
+			}
+		}
+		return result
+    }
+}
