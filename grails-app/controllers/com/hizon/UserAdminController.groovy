f002699d425b155b9db91d5bfe4dbea4cf8e7477
Lorence del Rosario <lorence.delrosario@orangeandbronze.com>
2014-03-27 01:31:17 +0800

    added user admin dashboard

--- grails-app/controllers/com/hizon/UserAdminController.groovy
+++ grails-app/controllers/com/hizon/UserAdminController.groovy
@@ -5,6 +5,8 @@ import org.springframework.dao.DataIntegrityViolationException
 class UserAdminController {
 
     static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+	
+	def springSecurityService
 
     def index() {
         redirect(action: "list", params: params)
@@ -102,4 +104,12 @@ class UserAdminController {
             redirect(action: "show", id: id)
         }
     }
+	
+	def dashboard(long id){
+		def user = (User) springSecurityService.getCurrentUser()
+		def loggedInUserAdmin = UserAdmin.findByUser(user)
+		
+		[userAdminId: loggedInUserAdmin.id]
+		
+	}
 }
