c22484aa1f3e0b8a0c8d0cd3d4d4486fb04bfb3f
Alec <alecgonzales@gmail.com>
2014-03-27 04:44:48 +0800

    Added method to get a random color

---
+++ grails-app/services/com/hizon/ColorService.groovy
@@ -0,0 +1,22 @@
+package com.hizon
+
+class ColorService {
+	def themeService
+	
+    def getRandomColorFromThemesContainingColors(List<Color> colors) {
+		Color result = null
+		Theme theme = themeService.getRandomThemeContainingColors(colors)
+		if (theme != null) {
+			def themeColors = theme.colors
+			for (Color color: colors) {
+				themeColors.remove(color)
+			}
+			
+			if (themeColors.size() > 0) {
+				int randomNumber = new Random().nextInt(themeColors.size())
+				result = themeColors.asList().get(randomNumber)
+			}
+		}
+		return result
+    }
+}
---
+++ test/integration/com/hizon/ColorServiceTests.groovy
@@ -0,0 +1,65 @@
+package com.hizon
+
+import static org.junit.Assert.*
+
+import java.util.Set;
+
+import org.junit.*
+
+class ColorServiceTests {
+	ColorService colorService = new ColorService()
+	Color red
+	Color green
+	Color blue
+	Color yellow
+	Color orange
+	
+	Level level
+	
+	Set<Color> rgb
+	Set<Color> rby
+	Set<Color> ryo
+	Set<Color> yo
+	
+	Theme rgbTheme
+	Theme rbyTheme
+	Theme ryoTheme
+	Theme yoTheme
+	
+	@Before
+	void setup() {
+		red = new Color(name: "Red", hex: "R", status: Status.ACTIVE).save(failOnError: true)
+		green = new Color(name: "Green", hex: "G", status: Status.ACTIVE).save(failOnError: true)
+		blue = new Color(name: "Blue", hex: "B", status: Status.ACTIVE).save(failOnError: true)
+		yellow = new Color(name: "Yellow", hex: "Y", status: Status.ACTIVE).save(failOnError: true)
+		orange = new Color(name: "Orange", hex: "O", status: Status.ACTIVE).save(failOnError: true)
+		assertEquals 5, Color.list().size()
+		
+		level = new Level(name: "1", status: Status.ACTIVE).save(failOnError: true)
+		
+		rgb = [red, green, blue]
+		rby = [red, blue, yellow]
+		ryo = [red, yellow, orange]
+		yo = [yellow, orange]
+		
+		rgbTheme = new Theme(name: "rgb", level: level, status: Status.ACTIVE, colors: rgb).save(failOnError: true)
+		rbyTheme = new Theme(name: "rby", level: level, status: Status.ACTIVE, colors: rby).save(failOnError: true)
+		ryoTheme = new Theme(name: "ryo", level: level, status: Status.ACTIVE, colors: ryo).save(failOnError: true)
+		yoTheme = new Theme(name: "yo", level: level, status: Status.ACTIVE, colors: yo).save(failOnError: true)
+		assertEquals 4, Theme.list().size()
+	}
+	
+	@Test
+    void getRandomColorFromThemesContainingColors() {
+		Color color = colorService.getRandomColorFromThemesContainingColors(yo.asList())
+		
+		assertNotNull(color)
+    }
+	
+	@Test
+	void getRandomColorFromThemesContainingColorsNone() {
+		Color color = colorService.getRandomColorFromThemesContainingColors(ryo.asList())
+
+		assertNull(color)
+	}
+}
